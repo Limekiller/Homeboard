d26edc45f6609c9688e169b07b0be5826187e7cd
root <root@benk-media-server.goshen.edu>
2018-05-25 01:43:36 -0400

    Clock widget, delete widgets, UI improvements

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,6 +1,9 @@
 widg_id = 1;
 var scale_factor;
+
 jQuery(document).ready(function () {
+
+    // Show Logout on profile image hover
     $(".acc-name").hover(function(){
             $(this).addClass('acc-name-hover');
             $('#acc-edit').css('margin-left', '50px');
@@ -10,18 +13,24 @@ jQuery(document).ready(function () {
             $('#acc-edit').css('margin-left', '');
             $('.acc-logout').removeClass('acc-logout-hover');
     });
+
+    // Enable edit mode
     $("#acc-edit").on("click", function () {
         $('#header2').css('margin-top', '-92vh');
         $('#header1').css('margin-top', '-90px');
         $('.widget').draggable('enable');
         $('.widget').resizable('enable');
     });
+
+    // Discard changes
     $("#edit_discard").on("click", function () {
         $('#header2').css('margin-top', '');
         $('#header1').css('margin-top', '');
         $('.widget').draggable('disable');
         $('.widget').resizable('disable');
     });
+
+    // Show widgets screen
     $("#edit_widgets").on("click", function () {
         if ($('#header2').css('margin-top') == "0px") {
             $('#header2').css('margin-top', '-92vh');
@@ -29,36 +38,60 @@ jQuery(document).ready(function () {
             $('#header2').css('margin-top', '0px');
         }
     });
+
+    // Select a widget
     $('.ws-widget').on('click', function() {
-        var widget_title = $(this).html();
-        $('#widget-area').append("<div class='widget'><div id='widg_"+widg_id+"' class='widget-i'></div></div>");
-        $('#widg_'+widg_id).load("https://homeboard.bryceyoder.com/widget/"+widget_title);
-        $('.widget').resizable({
-            helper: "ui-resizable-helper",
-            resize(event, ui) {
-                var $el = ui.element.children('.widget-i');
-                var elHeight = $el.outerHeight();
-                var elWidth = $el.outerWidth();
 
-                var scale;
-                scale = Math.min(
-                    ui.size.width / elWidth,
-                    ui.size.height / elHeight
-                );
+        if ($(this).attr('name') != undefined) {
+            $(this).css('background-color', 'rgba(45,45,45,1)');
+            $('#'+$(this).attr('name')).parent().remove();
+            $(this).removeAttr('name');
+        } else {
 
-                scale_factor = scale;
-            },
-            stop(event, ui){
-                var $el = ui.element.children('.widget-i');
-                $el.css({
-                    transform: "translate(-50%, -50%) "+ "scale("+ scale_factor + ")"
-                });
-            },
-            grid: 100});
-        $('.widget').draggable({
-            grid: [100, 100],
+            // Get widget title and ID
+            var widget_title = $(this).html();
+            temp_widg_id = widg_id;
+
+            $(this).css('background-color', 'red');
+            $(this).attr('name', 'widg_'+temp_widg_id);
+
+            // Add to page, load code, and fire init function that it should contain
+            $('#widget-area').append("<div class='widget'><div id='widg_"+widg_id+"' class='widget-i'></div></div>");
+            $('#widg_'+widg_id).load("https://homeboard.bryceyoder.com/widget/"+widget_title, function () {
+                init('widg_'+temp_widg_id);
             });
-        widg_id++;
-    });
 
+            // Enable resizability
+            $('.widget').resizable({
+                helper: "ui-resizable-helper",
+                resize(event, ui) {
+
+                    // Allow automatic scaling within widget
+                    var $el = ui.element.children('.widget-i');
+                    var elHeight = $el.outerHeight();
+                    var elWidth = $el.outerWidth();
+                    var scale;
+                    scale = Math.min(
+                        ui.size.width / elWidth,
+                        ui.size.height / elHeight
+                    );
+                    scale_factor = scale;
+                },
+
+                // Don't apply scaling until mouse released
+                stop(event, ui){
+                    var $el = ui.element.children('.widget-i');
+                    $el.css({
+                        transform: "translate(-50%, -50%) "+ "scale("+ scale_factor + ")"
+                    });
+                },
+                grid: 100});
+
+            // Enable Draggability
+            $('.widget').draggable({
+                grid: [100, 100],
+                });
+            widg_id++;
+        }
+    });
 });
---
+++ app/widgets/Clock/base.js
@@ -0,0 +1,42 @@
+function init(widg_id){
+    updateClock();
+    setInterval(function() { updateClock(); }, 1000);
+    var element = document.getElementById('clock');
+    new ResizeSensor(element, function() {
+        if (element.clientWidth < 500) {
+            $("#clock2").css('display', '');
+            $("#clock1").css('display', '');
+        }
+        else {
+            $("#clock2").css('display', 'none');
+            $("#clock1").css('display', 'inherit');
+            $("#week").css('width', $('#clock1_time').css('width'));
+        }
+    });
+
+}
+
+
+function updateClock()
+{
+    var currentTime = new Date();
+
+    var currentHours = currentTime.getHours ( );
+    var currentMinutes = currentTime.getMinutes ( );
+    var currentSeconds = currentTime.getSeconds ( );
+    var currentDay = currentTime.getDay()-1;
+    var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+
+    currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+    currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+    currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+    currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+    currentHours = (currentHours < 10 ? "0" : "" ) + currentHours;
+
+    var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+    $("#clock1_time").html(currentTimeString);
+    var currentTimeString = currentHours + "<br>" + currentMinutes;
+    $("#clock2_time").html(currentTimeString);
+    $("#week").children().eq(currentDay).css('color', 'rgba(255,255,255,1)');
+};
